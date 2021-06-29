d3c8689e80a58a2d8d6bf43ecc4071484c856894
Shota Iuchi <shotaiuchi.develop@gmail.com>
2021-06-30 00:09:33 +0900

    hlsサーバーテスト

--- src/pages/hlsjs/HlsjsPage.tsx
+++ src/pages/hlsjs/HlsjsPage.tsx
@@ -5,7 +5,8 @@ const HlsjsPage = () => {
   useEffect(() => {
     if (Hls.isSupported()) {
       const video = document.getElementById("video") as HTMLMediaElement;
-      const src = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8";
+      // const src = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8";
+      const src = "http://localhost:9900/output.m3u8";
       const hls = new Hls();
       hls.loadSource(src);
       hls.attachMedia(video);
