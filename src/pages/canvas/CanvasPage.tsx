ea24c3bdbb7923ff6f5d1f241b483b7c39f56f86
Shota Iuchi <shotaiuchi.develop@gmail.com>
2021-06-29 21:31:06 +0900

    canvas描画アニメーション

--- src/pages/canvas/CanvasPage.tsx
+++ src/pages/canvas/CanvasPage.tsx
@@ -1,9 +1,42 @@
-import { Layout } from "antd";
+import { useEffect, useRef } from "react";
 
 const CanvasPage = () => {
-  return (
-    <Layout style={{ padding: 10, backgroundColor: "#ffccff" }}>CANVAS</Layout>
-  );
+  const canvasRef = useRef(null);
+
+  const getContext = (): CanvasRenderingContext2D => {
+    const canvas: any = canvasRef.current;
+    return canvas?.getContext("2d");
+  };
+
+  const images = [
+    "https://molcar-anime.com/wp-content/themes/molcar-anime/images/top/chara/chara-01.png",
+    "https://molcar-anime.com/wp-content/themes/molcar-anime/images/top/chara/chara-02.png",
+    "https://molcar-anime.com/wp-content/themes/molcar-anime/images/top/chara/chara-03.png",
+    "https://molcar-anime.com/wp-content/themes/molcar-anime/images/top/chara/chara-04.png",
+    "https://molcar-anime.com/wp-content/themes/molcar-anime/images/top/chara/chara-05.png",
+  ];
+
+  useEffect(() => {
+    var count = 0;
+    const ctx: CanvasRenderingContext2D = getContext();
+    const interval = (v: any) => {
+      if (ctx === null) {
+        return;
+      }
+      const img = new Image();
+      img.src = images[count];
+      count = (count + 1) % images.length;
+      img.onload = () => {
+        ctx.fillStyle = "#cccccc";
+        ctx.fillRect(0, 0, 200, 200);
+        ctx.drawImage(img, 0, 0, 100, 100);
+        ctx.save();
+      };
+    };
+    setInterval(interval, 1000 / 60);
+  });
+
+  return <canvas ref={canvasRef}></canvas>;
 };
 
 export default CanvasPage;
